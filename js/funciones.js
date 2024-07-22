933d4a56f6189f786196289676ba820a17472e16
IsMa <itorre13@hotmail.com>
2024-07-22 10:23:40 +0200

    integración API lista

--- js/funciones.js
+++ js/funciones.js
@@ -2,11 +2,38 @@ const contTareas = document.querySelector(".tareas");
 const formulario = document.querySelector("form");
 const inputTexto = document.querySelector('form input[type="text"]');
 
+// CARGA INICIAL DE LOS DATOS
+fetch("http://localhost:4000/tareas")
+.then(respuesta => respuesta.json())
+.then(tareas => {
+    tareas.sort((a,b) => a.id - b.id).forEach(({id,tarea,terminada}) => { // esto las ordena descendientemente y devuelve al array ordenado. Y por cada una de esas tareas con el forEach queremos hacer algo
+        new Tarea(id,tarea,terminada,contTareas); // por cada tarea que me llegue del back pintaré una tarea en el front
+    }); 
+});
+
+
 formulario.addEventListener("submit", evento => {
     evento.preventDefault();
     
     if(inputTexto.value.trim() != ""){
-        new Tarea(1,inputTexto.value.trim(),false,contTareas);
-        inputTexto.value = "";
+        
+        let tarea = inputTexto.value.trim();
+
+        fetch("http://localhost:4000/tareas/nueva", {
+            method : "POST",
+            body : JSON.stringify({tarea}),
+            headers : {
+                "Content-type" : "application/json"
+            }
+        })
+        .then(respuesta => respuesta.json())
+        .then(({id,error}) => { // las dos posibles propiedades de esta respuesta son id y error. Si todo salió bien es id y si algo falló es error
+            if(!error){ // si no hay error
+                new Tarea(id,tarea,false,contTareas);
+                return inputTexto.value = ""; // para que se limpie el campo
+            }
+            console.log("..mostrar error al usuario");
+        });
+
     }
 });
--- js/tarea.js
+++ js/tarea.js
@@ -39,7 +39,9 @@ class Tarea{
         botonEstado.appendChild(document.createElement("span"));
 
         botonEstado.addEventListener("click",() => {
-            this.actualizarEstado().then(() => botonEstado.classList.toggle("terminada"));
+            this.actualizarEstado()
+            .then(() => botonEstado.classList.toggle("terminada"))
+            .catch(() => console.log("...mostrar error a usuario"));
         });
 
         this.DOM.appendChild(textoTarea);
@@ -49,33 +51,61 @@ class Tarea{
         this.DOM.appendChild(botonEstado);
         contenedor.appendChild(this.DOM);
     }
-    actualizarTexto(){
-        if(this.editando){
-           let textoTemporal = this.DOM.children[1].value.trim();
+    async actualizarTexto(){
+        if(this.editando){ // si estamos editando
+           let textoTemporal = this.DOM.children[1].value.trim(); // extraemos el texto temporal del input
 
-           if(textoTemporal != "" & textoTemporal != this.texto){
-                this.texto = textoTemporal;
+           if(textoTemporal != "" & textoTemporal != this.texto){ // preguntamos si en efecto el usuario lo ha editado y la edición es correcta. No lo has dejado en blanco y has cambiado las cosas
+            let {error} = await fetch("http://localhost:4000/tareas/actualizar/1/" + this.id,{ // el error lo quiero de esperar de esta llamada a fetch 
+                    method : "PUT",
+                    body : JSON.stringify({ tarea : textoTemporal }),
+                    headers : {
+                        "Content-type" : "application/json"
+                    }
+                }).then(respuesta => respuesta.json()); // convertimos la respuesta en json
+
+                if(!error){ // si no hay error
+                    this.texto = textoTemporal; // mostrará el texto temporal
+                }else{ // si hay error
+                    console.log("..error al usuario"); // mostraremos un error al usuario
+                }
            }
         
-           this.DOM.children[1].classList.remove("visible");
+           this.DOM.children[1].classList.remove("visible"); // revierto toda la operación que ha activado la edición, ha cambiado el texto el botón, etc.
            this.DOM.children[0].innerText = this.texto;
            this.DOM.children[0].classList.add("visible");
            this.DOM.children[2].innerText = "editar";
  
         }else{
-            this.DOM.children[0].classList.remove("visible");
+            this.DOM.children[0].classList.remove("visible"); // si no es estabamos editando q es como empezamos por defecto, lo que hacemos es cambiar el DOM para que permita editar
             this.DOM.children[1].value = this.texto; //con setAttribute("value",this.texto) NO FUNCIONA. Hay que hacerlo de esa manera
             this.DOM.children[1].classList.add("visible");
             this.DOM.children[2].innerText = "guardar";
         }
-        this.editando = !this.editando;
+        this.editando = !this.editando; // y pase lo que pase hacemos un toggle. Si estabas editando o no editando
     }
-    borrarTarea(){
-        this.DOM.remove();
-    }
+
     actualizarEstado(){
         return new Promise((ok,ko) => {
-            ok();
+            fetch("http://localhost:4000/tareas/actualizar/2/" + this.id, {// 2 es la operación del estado. Concatenamos el id xq siempre va al final de la url
+                method : "PUT"
+            })
+            .then(respuesta => respuesta.json())
+            .then(({error}) => {
+                !error ? ok() : ko(); // si no hay error cumplo la promesa y de lo contrario la rechazo
+            });
         });
+    }    
+    borrarTarea(){
+        fetch("http://localhost:4000/tareas/borrar/" + this.id, {// concatenamos el id xq siempre va al final de la url
+            method : "DELETE" // sólo metodo porque la informacion del cuerpo de la peticion y las cabeceras va en la URL
+            })
+            .then(respuesta => respuesta.json())
+            .then(({resultado,error}) => { // se que puedo recibir resultado y un posible error. En este frontend el resultado es irrelevante, podríamos quedarnos sólo con el error.
+                if(!error){ // si no tengo ningún error
+                    return this.DOM.remove(); // se elemina del front
+                }
+                console.log("...mostrar error a usuario");
+            });
     }
 }
