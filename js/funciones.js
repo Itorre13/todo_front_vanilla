bf4d7f45c0aadc314e3ed4da98032a81250eafdd
IsMa <itorre13@hotmail.com>
2024-10-01 13:05:14 +0200

    todo_front 100%

--- js/funciones.js
+++ js/funciones.js
@@ -3,10 +3,10 @@ const formulario = document.querySelector("form");
 const inputTexto = document.querySelector('form input[type="text"]');
 
 // CARGA INICIAL DE LOS DATOS
-fetch("https://api-todo-postgres-a32c.onrender.com/tareas")
+fetch("https://localhost:4000/tareas")
 .then(respuesta => respuesta.json())
 .then(tareas => {
-    tareas.sort((a,b) => a.id - b.id).forEach(({id,tarea,terminada}) => { // esto las ordena descendientemente y devuelve al array ordenado. Y por cada una de esas tareas con el forEach queremos hacer algo
+    tareas.sort((a,b) => a.id - b.id).forEach(({id,tarea,terminada}) => { // esto las ordena ascendentemente y devuelve el array ordenado. Y apartir de ese array le diremos que queremos hacer algo. Que por cada una de esas tareas con el forEach queremos hacer algo
         new Tarea(id,tarea,terminada,contTareas); // por cada tarea que me llegue del back pintaré una tarea en el front
     }); 
 });
@@ -19,7 +19,7 @@ formulario.addEventListener("submit", evento => {
         
         let tarea = inputTexto.value.trim();
 
-        fetch("https://api-todo-postgres-a32c.onrender.com/tareas/nueva", {
+        fetch("https://localhost:4000/tareas/nueva", {
             method : "POST",
             body : JSON.stringify({tarea}),
             headers : {
